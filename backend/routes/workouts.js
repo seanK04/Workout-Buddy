0adf9d142297e3d5f02be9e4401cc427a8f45e1e
sehoonk2004 <107429645+sehoonk2004@users.noreply.github.com>
2024-08-09 16:05:29 -0400

    Finished Tutorial #8

---
+++ backend/routes/workouts.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const {
+    createWorkout,
+    getWorkouts,
+    getWorkout,
+    deleteWorkout,
+    updateWorkout
+} = require('../controllers/workoutController')
+const router = express.Router()
+
+//GET all workouts
+router.get('/', getWorkouts)
+
+//GET single workout
+router.get('/:id', getWorkout)
+
+//POST a new workout
+router.post('/', createWorkout)
+
+//DELETE a workout
+router.delete('/:id', deleteWorkout)
+
+//UPDATE a workout
+router.patch('/:id', updateWorkout)
+
+module.exports = router
